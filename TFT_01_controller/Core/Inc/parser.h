33a3f0a92514aa27c7cf79cfa9632856bf5973c1
grad73 <gradowski.kam@gmail.com>
2021-07-07 09:41:22 +0200

    dodane funkcje do parsowania różnych stanów

--- TFT_01_controller/Core/Inc/parser.h
+++ TFT_01_controller/Core/Inc/parser.h
@@ -13,11 +13,21 @@ typedef enum
 	IDLE = 0, 				//0
 	INITIALIZATION,			//initializacja systemu, stan po restarcie lub włączeniu
 	PARAMETERS,				//wyswietla aktualne parametry wody i powietrza
-	SW_CONTROLING,			//ekran z ręcznym włączaniem i wyłączaniem przerzutników
+	SW_CONTROLLING,			//ekran z ręcznym włączaniem i wyłączaniem przerzutników
 	LIGHT_CONTROLLING, 		//ekran z ręcznym sterowaniem światłami
 	LIGHT_TIMERS,			//ustawianie czasu włłączania i wyłączania świateł
 	CONNECTION_ERROR		//błąd połączenia UART z drugim uC
 
 } CONTROLLER_STATE;
+
+void SwitchControllerState();
+
+void IdleRoutine(uint8_t * AqaParameters);
+void InitializationRoutine(uint8_t * AqaParameters);
+void ParametersRoutine(uint8_t * AqaParameters);
+void SwitchControlingRoutine(uint8_t * AqaParameters);
+void LightControllingRoutine(uint8_t * AqaParameters);
+void LightTimersRoutine(uint8_t * AqaParameters);
+void ConnectionErrorRoutine(uint8_t * AqaParameters);
 
 #endif /* INC_PARSER_H_ */
--- TFT_01_controller/Core/Src/parser.c
+++ TFT_01_controller/Core/Src/parser.c
@@ -4,5 +4,75 @@
  *  Created on: Jul 7, 2021
  *      Author: grad_
  */
+#include "main.h"
+#include "parser.h"
+
+CONTROLLER_STATE ControlerRegulator;
+uint8_t AqaParameters[3];
+
+void SwitchControllerState()
+{
+	switch(ControlerRegulator){
+		case IDLE:
+			IdleRoutine(AqaParameters);
+			break;
+		case INITIALIZATION:
+			InitializationRoutine(AqaParameters);
+			break;
+		case PARAMETERS:
+			ParametersRoutine(AqaParameters);
+			break;
+		case SW_CONTROLLING:
+			SwitchControlingRoutine(AqaParameters);
+			break;
+		case LIGHT_CONTROLLING:
+			LightControllingRoutine(AqaParameters);
+			break;
+		case LIGHT_TIMERS:
+			LightTimersRoutine(AqaParameters);
+			break;
+		case CONNECTION_ERROR:
+			ConnectionErrorRoutine(AqaParameters);
+			break;
+		default:
+			break;
+
+		}
+}
+
+void IdleRoutine(uint8_t * AqaParameters)
+{
+
+}
+
+void InitializationRoutine(uint8_t * AqaParameters)
+{
+
+}
+
+void ParametersRoutine(uint8_t * AqaParameters)
+{
+
+}
+
+void SwitchControlingRoutine(uint8_t * AqaParameters)
+{
+
+}
+
+void LightControllingRoutine(uint8_t * AqaParameters)
+{
+
+}
+
+void LightTimersRoutine(uint8_t * AqaParameters)
+{
+
+}
+
+void ConnectionErrorRoutine(uint8_t * AqaParameters)
+{
+
+}
 
 
