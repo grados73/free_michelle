718726b5e1f1e55d1001dc5a26665b815f8343e5
grad73 <gradowski.kam@gmail.com>
2021-07-07 09:19:51 +0200

    dodany plik do parsowania i wstępne stany kontrolera

---
+++ TFT_01_controller/Core/Inc/parser.h
@@ -0,0 +1,22 @@
+/*
+ * parser.h
+ *
+ *  Created on: Jul 7, 2021
+ *      Author: grad_
+ */
+
+#ifndef INC_PARSER_H_
+#define INC_PARSER_H_
+
+typedef enum
+{
+	IDLE = 0, 				//0
+	INITIALIZATION,			//initializacja systemu, stan po restarcie lub włączeniu
+	PARAMETERS,				//wyswietla aktualne parametry wody i powietrza
+	SW_CONTROLING,			//ekran z ręcznym włączaniem i wyłączaniem przerzutników
+	LIGHT_CONTROLLING, 		//ekran z ręcznym sterowaniem światłami
+	LIGHT_TIMERS			//ustawianie czasu włłączania i wyłączania świateł
+
+} CONTROLLER_STATE;
+
+#endif /* INC_PARSER_H_ */
---
+++ TFT_01_controller/Core/Src/parser.c
@@ -0,0 +1,8 @@
+/*
+ * parser.c
+ *
+ *  Created on: Jul 7, 2021
+ *      Author: grad_
+ */
+
+
