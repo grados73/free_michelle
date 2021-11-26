56a50c86b051e90fb256ebe07184f8ab64af614a
grad73 <gradowski.kam@gmail.com>
2021-11-26 20:33:45 +0100

    Dodany prototyp projektu do dodania modułu dotykowego

---
+++ TFT_01_controller_TOUCH/Core/Inc/parser.h
@@ -0,0 +1,87 @@
+/*
+ * parser.h
+ *
+ *  Created on: Jul 7, 2021
+ *      Author: grad_
+ */
+
+#ifndef INC_PARSER_H_
+#define INC_PARSER_H_
+
+#define NUMBER_OF_RELAY 4
+
+typedef enum
+{
+	IDLE = 0, 				//0
+	INITIALIZATION,			//initializacja systemu, stan po restarcie lub włączeniu
+	PARAMETERS,				//wyswietla aktualne parametry wody i powietrza
+	SW_CONTROLLING,			//ekran z ręcznym włączaniem i wyłączaniem przerzutników
+	LIGHT_CONTROLLING, 		//ekran z ręcznym sterowaniem światłami
+	LIGHT_TIMERS,			//ustawianie czasu włłączania i wyłączania świateł
+	CONNECTION_ERROR		//błąd połączenia UART z drugim uC
+
+} CONTROLLER_STATE;
+
+typedef struct
+{
+	char status[24];
+	float temperatura1;
+	float cisnienie1;
+	uint8_t przekazniki[NUMBER_OF_RELAY];
+
+} second_uc_data;
+
+
+
+//////////////////////////////////////// ERRORS //////////////////////////////////////////////////////////////////////////////////////////////////////
+#define LED_FORMAT_ERROR "LED=ERR_FORMAT\n"
+#define TEMPERATURE_FORMAT_ERROR "ATEMP=ERR_FORMAT\n"
+#define PRESURE_FORMAT_ERROR "APRES=ERR_FORMAT\n"
+#define CHANGE_RELAY_STATE_FORMAT_ERROR "ACHSTATE=ERR_FORMAT\n"
+#define CHANGE_RELAY_STATE_NUMBER_ERROR "ACHSTATE=ERR_NUMBER\n"
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+void SwitchControllerState();
+void IdleRoutine(uint8_t * AqaParameters);
+void InitializationRoutine(uint8_t * AqaParameters);
+void ParametersRoutine(uint8_t * AqaParameters);
+void SwitchControlingRoutine(uint8_t * AqaParameters);
+void LightControllingRoutine(uint8_t * AqaParameters);
+void LightTimersRoutine(uint8_t * AqaParameters);
+void ConnectionErrorRoutine(uint8_t * AqaParameters);
+
+#include "uartdma.h"
+#include "TFT_ILI9341.h"
+#include "GFX_Color.h"
+#include "GFX_EnhancedFonts.h"
+#include "EnhancedFonts/arialBlack_20ptFontInfo.h"
+#include "EnhancedFonts/ArialBlack_28pts_bold.h"
+#include "string.h"
+#include "stdio.h"
+#include "functions.h"
+
+#define BUFFOR_SIZE 65
+#define FRAME_SIZE 16
+#define SOURCE_STATE_LENGTH 10
+
+#define DATA_SIZE_FR 4
+
+#define MY_ADRESS 2 //Adres danego urządzenia
+
+void UART_ParseLine(UARTDMA_HandleTypeDef *huartdma);
+void UART_ParseLED();
+void UART_ParseAnswStatus();
+void UART_ParseAnswTemp();
+void UART_ParseAnswPres();
+void UART_ParseAnswChangeRelayState();
+
+
+void SwitchMotorRegular();
+
+void PodajStatusRoutine();
+void PodajTemperatureRoutine(uint8_t NrCzujnika);
+void PodajCisnienieRoutine(uint8_t NrCzujnika);
+void ZmienStanPrzekRoutine(uint8_t NrPrzekaznika, uint8_t Stan);
+
+#endif /* INC_PARSER_H_ */
---
+++ TFT_01_controller_TOUCH/Core/Src/parser.c
@@ -0,0 +1,230 @@
+/**
+  ******************************************************************************
+  * @file           : parser.c
+  * @project		: free_michelle
+  * @author			: grados73 - https://github.com/grados73
+  * @purpose		: plik z funkcjami parsowania wiadomosci przychodzacych z UART
+  ******************************************************************************
+  **/
+#include "main.h"
+#include "parser.h"
+#include "string.h"
+#include "uartdma.h"
+#include "stdlib.h"
+#include "stdio.h"
+#include "functions.h"
+
+extern UARTDMA_HandleTypeDef huartdma2;
+char Message[BUFFOR_SIZE]; // Transmit buffer
+char MyName[32] = {"SLAVE1"}; // Name string
+uint8_t ChangingStateFlag;
+extern struct Measurements BMPResults;
+float CTemp = 0.0;
+float Cpres = 0.0;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////// GLOWNA FUNKCJA PARSOWANIA //////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+ * Parsing headers:
+ * 		LED=1\n 		// LED On
+ * 		LED=0\n 		// LED Off
+ * 		STATE=?\n		// Jaki jest stan uC
+ * 		TEMP=1\n		// Jaka jest temperatura 1 czujnika
+ * 		PRES=1\n		// Jakie jest cisnienie 1 czujnika
+ * 		CHSTATE=1,0\n	// Zmien stan przekaznika 1 na wylaczony
+ * 		CHSTATE=2,1\n	// Zmien stan przekaznika 2 na wlaczony
+ */
+
+void UART_ParseLine(UARTDMA_HandleTypeDef *huartdma)
+{
+	char BufferReceive[BUFFOR_SIZE];
+
+	if(!UARTDMA_GetLineFromReceiveBuffer(huartdma, BufferReceive))
+	{
+		// Header
+		char* ParsePointer = strtok(BufferReceive, "="); // LED\0   1\0
+		// ParsePointer == LED\0
+
+	  if(strcmp(ParsePointer, "LED") == 0)
+	  {
+		  UART_ParseLED();
+	  }
+	  else if (strcmp(ParsePointer, "ASTATE") == 0)
+	  {
+		  UART_ParseAnswStatus();
+	  }
+	  else if (strcmp(ParsePointer, "ATEMP") == 0)
+	  {
+		  UART_ParseAnswTemp();
+	  }
+	  else if (strcmp(ParsePointer, "APRES") == 0)
+	  {
+		  UART_ParseAnswPres();
+	  }
+	  else if (strcmp(ParsePointer, "ACHSTATE") == 0)
+	  {
+		  UART_ParseAnswChangeRelayState();
+	  }
+	  //TODO: DODAC OBSLUGE PARSOWANIA BLEDOW
+	}
+}
+
+
+/////////////////////////////////////////// FUNKCJA PARSOWANIA USER LED ////////////////////////////////////////////////////////////////////////////
+
+void UART_ParseLED()
+{
+	uint8_t LedState; // Received state variable
+
+	char* ParsePointer = strtok(NULL, ","); // Look for next token or end of string
+	// Should be now: ParsePointer == 1'\0'
+
+	if(strlen(ParsePointer) > 0) // If string exists
+	{
+		if(ParsePointer[0] < '0' || ParsePointer[0] > '9') // Chceck if there are only numbers
+		{
+			UARTDMA_Print(&huartdma2, LED_FORMAT_ERROR); // Print message
+			return;	// And exit parsing
+		}
+
+		LedState = atoi(ParsePointer); // If there are no chars, change string to integer
+
+		if(LedState == 1) // LED ON
+		{
+			HAL_GPIO_WritePin(BP_USER_LED_GPIO_Port, BP_USER_LED_Pin, GPIO_PIN_RESET);
+			UARTDMA_Print(&huartdma2, "LED=On\r\n");
+
+		}
+		else if(LedState == 0) // LED OFF
+		{
+			HAL_GPIO_WritePin(BP_USER_LED_GPIO_Port, BP_USER_LED_Pin, GPIO_PIN_SET);
+			UARTDMA_Print(&huartdma2, "LED=Off\r\n");
+		}
+		else // Wrong state number
+		{
+			UARTDMA_Print(&huartdma2, LED_FORMAT_ERROR);
+		}
+	}
+}
+
+void UART_ParseAnswStatus()
+{
+	//TODO: Dodac obsluge statusu urzadzenia
+}
+
+
+void UART_ParseAnswTemp()
+{
+	char* ParsePointer = strtok(NULL, ",");
+	uint8_t Len;
+	if(strlen(ParsePointer) > 0) // If string exists
+	{
+		CTemp = atof(ParsePointer); // If there are no chars, change string to integer
+		Len = sprintf((char*)Msg, "Temp. zewn: %.2f`C", CTemp);
+		EF_PutString(Msg, 20, 50, ILI9341_BLACK, BG_COLOR, ILI9341_LIGHTGREY);
+		UARTDMA_Print(&huartdma2, "TEMPUPSUC\n");
+		Len++;
+	}
+}
+
+void UART_ParseAnswPres()
+{
+	char* ParsePointer = strtok(NULL, ",");
+	uint8_t Len;
+	if(strlen(ParsePointer) > 0) // If string exists
+	{
+		Cpres = atof(ParsePointer); // If there are no chars, change string to integer
+		Len = sprintf((char*)Msg, "Ciśnienie: %.1fhPa", Cpres);
+		EF_PutString(Msg, 20, 140, ILI9341_BLACK, BG_COLOR, ILI9341_LIGHTGREY);
+		UARTDMA_Print(&huartdma2, "PRESUPSUC\n");
+		Len++;
+	}
+}
+
+
+
+void UART_ParseAnswChangeRelayState()
+{
+
+}
+
+
+
+
+
+
+
+///////////////////////////////////////////// MASZYNA STANOW ////////////////////////////////
+
+CONTROLLER_STATE ControlerRegulator;
+uint8_t AqaParameters[3];
+
+void SwitchControllerState()
+{
+	switch(ControlerRegulator){
+		case IDLE:
+			IdleRoutine(AqaParameters);
+			break;
+		case INITIALIZATION:
+			InitializationRoutine(AqaParameters);
+			break;
+		case PARAMETERS:
+			ParametersRoutine(AqaParameters);
+			break;
+		case SW_CONTROLLING:
+			SwitchControlingRoutine(AqaParameters);
+			break;
+		case LIGHT_CONTROLLING:
+			LightControllingRoutine(AqaParameters);
+			break;
+		case LIGHT_TIMERS:
+			LightTimersRoutine(AqaParameters);
+			break;
+		case CONNECTION_ERROR:
+			ConnectionErrorRoutine(AqaParameters);
+			break;
+		default:
+			break;
+
+		}
+}
+
+void IdleRoutine(uint8_t * AqaParameters)
+{
+
+}
+
+void InitializationRoutine(uint8_t * AqaParameters)
+{
+	// TODO - DODAĆ OBSŁUGĘ RETURNA, JAK ZWRACA 1 TO PRZECHODZI DO PARAMETERS A JAK NIE TO DO ERRORS
+	system_init();
+}
+
+void ParametersRoutine(uint8_t * AqaParameters)
+{
+
+}
+
+void SwitchControlingRoutine(uint8_t * AqaParameters)
+{
+
+}
+
+void LightControllingRoutine(uint8_t * AqaParameters)
+{
+
+}
+
+void LightTimersRoutine(uint8_t * AqaParameters)
+{
+
+}
+
+void ConnectionErrorRoutine(uint8_t * AqaParameters)
+{
+
+}
+
+
